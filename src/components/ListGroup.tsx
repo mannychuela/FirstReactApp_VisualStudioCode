8e5401ffa3908457962ab01467ae781f256ddb1e
gsb52000 <144732171+mannychuela@users.noreply.github.com>
2025-03-01 18:15:08 -0400

    Added src files

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,30 @@
+function ListGroup() {
+  const items = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+    "Philadelphia",
+  ];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
